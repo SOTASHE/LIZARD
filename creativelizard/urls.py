95874f508724ad065c108354a85b9783131fe272
Sinaye Sotashe <sinayesotatshe@gmail.com>
2021-09-06 17:58:15 +0200

    first commit

---
+++ creativelizard/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+from site_app.views import home
+
+urlpatterns = [
+    path(r'', home, name='homepage'),
+    path('tinymce/', include('tinymce.urls')),
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
