87d6bb31271487c22665bd1515f606c97b03a914
Sinaye Sotashe <sinayesotatshe@gmail.com>
2021-09-10 16:05:17 +0200

    updated settings

--- creativelizard/settings.py
+++ creativelizard/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zl)*t3dm%!45y25fn^ov%a==a2c6z6=#7f@qwq0txc2r&*9d*&'
-
+#SECRET_KEY = 'django-insecure-zl)*t3dm%!45y25fn^ov%a==a2c6z6=#7f@qwq0txc2r&*9d*&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['creative-l.herokuapp.com']
 
 
 # Application definition
